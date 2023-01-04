006fb064bf38e7163721463b367a574f62595126
elena malova <amelilp@yahoo.com>
2023-01-05 00:21:05 +0300

    Добавлены webp фото, некоторые правки стилизации и js

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,9 @@
 let navMain = document.querySelector('.navigation');
 let navToggle = document.querySelector('.navigation__toggle');
+let modal = document.querySelector('.modal-container');
+let cart = document.querySelector('.catalog__cart');
+let modalButton = document.querySelector('.modal__add');
+let order = document.querySelector('.promo__order');
 
 navMain.classList.remove('navigation--nojs');
 
@@ -10,5 +14,33 @@ navToggle.addEventListener('click', function () {
   } else {
     navMain.classList.add('navigation--closed');
     navMain.classList.remove('navigation--opened');
+  }
+});
+
+cart.addEventListener('click', function () {
+  if (modal.classList.contains('modal-container--closed')) {
+    modal.classList.remove('modal-container--closed');
+    modal.classList.add('modal-container--opened');
+  }
+});
+
+modalButton.addEventListener('click', function () {
+  if (modal.classList.contains('modal-container--opened')) {
+    modal.classList.remove('modal-container--opened');
+    modal.classList.add('modal-container--closed');
+  }
+});
+
+modal.addEventListener('click', function () {
+  if (modal.classList.contains('modal-container--opened')) {
+    modal.classList.remove('modal-container--opened');
+    modal.classList.add('modal-container--closed');
+  }
+});
+
+order.addEventListener('click', function () {
+  if (modal.classList.contains('modal-container--closed')) {
+    modal.classList.remove('modal-container--closed');
+    modal.classList.add('modal-container--opened');
   }
 });
